287841f6d68922f8d38879753d841c71ef374f11
Zeynep Pınar Kaya <zeynepinarkaya@gmail.com>
2020-12-10 20:48:55 +0000

    tüm sorular çözüldü, 3.soruya tekrar bakacağım

--- odev.js
+++ odev.js
@@ -5,9 +5,9 @@
 
 var person = {
   name: "John",
-  message: function () {    
-    console.log("Hello, " + this.name)
-  }
+  message: function () {
+    console.log("Hello, " + person.name)
+  }.bind(person)
 }
 
 var messageFunc = person.message
@@ -23,12 +23,12 @@ messageFunc();
   sonuclarini yazdiracak sekilde duzenleyiniz.
 */
 var numbers = {
-  numbers: [[10,20,30], 2],
-  multiply: function(){
-    this.numbers[0].map(function(number, numberIndex){
-        const result = number * this.numbers[1];
-        console.log(result)
-    })
+  numbers: [[10, 20, 30], 2],
+  multiply: function () {
+    this.numbers[0].map(function (number, numberIndex) {
+      const result = number * this.numbers[1];
+      console.log(result)
+    }.bind(numbers))
   }
 };
 
@@ -42,9 +42,14 @@ numbers.multiply();
   Ornek : isValidName("John") true donmeli
   Ornek : isValidName(" J ohn") false donmeli
 */
-function isValidName(name){
 
+function isValidName(name) {
+  let n = name.includes(" ", 0);
+  console.log(!n);
 }
+isValidName("Jo  hn");
+
+
 
 /*
   Odev 4:
@@ -58,8 +63,15 @@ function isValidName(name){
   Ornek: katilimSaati("3", 20) 60 sonucunu vermelidir.
   Ornek: katilimSaati("5", "30") 150 sonucunu vermelidir.
 */
-function katilimSaati(dersSayisi, dersSuresi){
-
+function katilimSaati(dersSayisi, dersSuresi) {
+  if (isNaN(parseInt(dersSayisi)) || isNaN(parseInt(dersSuresi))
+      || (dersSayisi <0 || dersSuresi <0)) {
+    throw new Error("hata, input formatı uygun değil!");
+  }
+  let toplam = parseInt(dersSayisi) * parseInt(dersSuresi);
+  return console.log(toplam);
 }
+
+katilimSaati(10, -1);
 
 
